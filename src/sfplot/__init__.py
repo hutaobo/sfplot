d43431ecf259afd3cbb9ae219be2c01f0c41e62d
Taobo Hu <taobo.hu@scilifelab.se>
2025-04-25 17:07:36 +0200

    Changes

--- src/sfplot/__init__.py
+++ src/sfplot/__init__.py
@@ -3,7 +3,8 @@
 __author__ = """Taobo Hu"""
 __email__ = 'taobo.hu@scilifelab.se'
 
-from .TCR_distance_heatmap import generate_TCR_distance_heatmap_from_df
+from .circle_heatmap import circle_heatmap
+from .compute_col_dendrogram_scores import compute_col_dendrogram_scores
 from .data_processing import load_xenium_data
 from .plotting import generate_cluster_distance_heatmap_from_adata
 from .plotting import generate_cluster_distance_heatmap_from_path
@@ -12,20 +13,21 @@ from .sfplot import split_B_by_distance_to_A
 from .Searcher_Findee_Score import compute_cophenetic_distances_from_adata
 from .Searcher_Findee_Score import compute_cophenetic_distances_from_df
 from .Searcher_Findee_Score import plot_cophenetic_heatmap
-from .compute_col_dendrogram_scores import compute_col_dendrogram_scores
 from .tbc_analysis import transcript_by_cell_analysis
+from .TCR_distance_heatmap import generate_TCR_distance_heatmap_from_df
 
 __all__ = [
-    "load_xenium_data",
+    "circle_heatmap",
+    "compute_col_dendrogram_scores",
+    "compute_cophenetic_distances_from_adata",
+    "compute_cophenetic_distances_from_df",
     "generate_cluster_distance_heatmap_from_adata",
     "generate_cluster_distance_heatmap_from_path",
     "generate_cluster_distance_heatmap_from_df",
-    "split_B_by_distance_to_A",
     "generate_TCR_distance_heatmap_from_df",
-    "compute_cophenetic_distances_from_adata",
-    "compute_cophenetic_distances_from_df",
+    "load_xenium_data",
     "plot_cophenetic_heatmap",
-    "compute_col_dendrogram_scores",
-    "transcript_by_cell_analysis"
+    "split_B_by_distance_to_A",
+    "transcript_by_cell_analysis",
     # 其他导出的函数或类
 ]
