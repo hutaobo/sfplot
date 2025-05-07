5c6397773d31f49c489f17eb421d196a272ae278
Taobo Hu <taobo.hu@scilifelab.se>
2025-05-07 14:19:41 +0200

    Changes

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
-    # … 其它配置 …
+    name="sfplot",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={
