22870c02556eecb2cb5c261a315ec864ac29f60f
Taobo Hu <thuac@connect.ust.hk>
2025-10-09 11:41:18 +0200

    add new feature

--- src/sfplot/__init__.py
+++ src/sfplot/__init__.py
@@ -1,37 +1,47 @@
 """Top-level package for sfplot."""
 
-__author__ = """Taobo Hu"""
-__email__ = 'taobo.hu@scilifelab.se'
+__author__ = "Taobo Hu"
+__email__ = "taobo.hu@scilifelab.se"
 
-from .Searcher_Findee_Score import compute_cophenetic_distances_from_adata
-from .Searcher_Findee_Score import compute_cophenetic_distances_from_df
-from .Searcher_Findee_Score import plot_cophenetic_heatmap
-from .TCR_distance_heatmap import generate_TCR_distance_heatmap_from_df
+# ---- public APIs (functions) ----
 from .circle_heatmap import circle_heatmap
-from .circular_dendrogram import plot_circular_dendrogram_pycirclize
 from .compute_col_dendrogram_scores import compute_col_dendrogram_scores
+from .Searcher_Findee_Score import (
+    compute_cophenetic_distances_from_adata,
+    compute_cophenetic_distances_from_df,
+    plot_cophenetic_heatmap,
+)
+from .TCR_distance_heatmap import generate_TCR_distance_heatmap_from_df
+from .plotting import (
+    generate_cluster_distance_heatmap_from_adata,
+    generate_cluster_distance_heatmap_from_df,
+    generate_cluster_distance_heatmap_from_path,
+)
 from .data_processing import load_xenium_data
-from .plotting import generate_cluster_distance_heatmap_from_adata
-from .plotting import generate_cluster_distance_heatmap_from_df
-from .plotting import generate_cluster_distance_heatmap_from_path
 from .sfplot import split_B_by_distance_to_A
 from .tbc_analysis import transcript_by_cell_analysis
 from .tbc_analysis_serial import transcript_by_cell_analysis_serial
 
+# ---- expose submodule so `sfplot.circular_dendrogram.*` works ----
+from . import circular_dendrogram
+from .circular_dendrogram import plot_circular_dendrogram_pycirclize
+
 __all__ = [
+    # functions
     "circle_heatmap",
-    "circular_dendrogram",
     "compute_col_dendrogram_scores",
     "compute_cophenetic_distances_from_adata",
     "compute_cophenetic_distances_from_df",
-    "generate_cluster_distance_heatmap_from_adata",
-    "generate_cluster_distance_heatmap_from_path",
-    "generate_cluster_distance_heatmap_from_df",
-    "generate_TCR_distance_heatmap_from_df",
-    "load_xenium_data",
     "plot_cophenetic_heatmap",
+    "generate_TCR_distance_heatmap_from_df",
+    "generate_cluster_distance_heatmap_from_adata",
+    "generate_cluster_distance_heatmap_from_df",
+    "generate_cluster_distance_heatmap_from_path",
+    "load_xenium_data",
     "split_B_by_distance_to_A",
     "transcript_by_cell_analysis",
     "transcript_by_cell_analysis_serial",
-    # 其他导出的函数或类
+    "plot_circular_dendrogram_pycirclize",
+    # submodules
+    "circular_dendrogram",
 ]
